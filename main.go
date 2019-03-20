72ad2c624ea0fdf25f360c65579931f8746071da
andykuszyk <andy.kuszyk@carfinance247.co.uk>
2019-03-20 17:36:13 +0000

    Added command for search

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
     app.Commands = []cli.Command{
         {
             Name: "clone",
-            Usage: "Clones git repositories for a given GitHub organisation to a given location. "+ 
+            Usage: "Clones git repositories for a given GitHub organisation to a given location. "+
                     "Assumes the user has SSH access to the organisation provided in --org",
             Action: commands.Clone,
             Flags: []cli.Flag {
@@ -29,6 +29,22 @@ func main() {
                 cli.StringFlag {
                     Name: "token",
                     Usage: "The Personal Access Token required to authenticate with the GitHub API",
+                },
+            },
+        },
+        {
+            Name: "search",
+            Usage: "Searches a directory of code repositories for references to entities, returning "+
+                   "those repositories that match, directly or indirectly",
+            Action: commands.Search,
+            Flags: []cli.Flag {
+                cli.StringFlag {
+                    Name: "deps",
+                    Usage: "The dependencies to search for, provided as a white-space separated list",
+                },
+                cli.StringFlag {
+                    Name: "dir",
+                    Usage: "The directory containing code repositories, in which to search",
                 },
             },
         },
