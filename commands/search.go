648dbb00c0f42dfd4ce4d978389efcda57647e84
andykuszyk <andy.kuszyk@carfinance247.co.uk>
2019-03-21 09:48:51 +0000

    First draft at search function

--- commands/search.go
+++ commands/search.go
@@ -6,32 +6,57 @@ import (
     "strings"
     "log"
     "io/ioutil"
+    "path/filepath"
 )
 
 type dependant struct {
     name string
-    parent *dep
+    parent *dependant
     repos []string
 }
 
-func searchChildren(parent string, dependants *[]dependant) {
+func (d *dependant) addRepo(repo string) {
+    d.repos = append(d.repos, repo)
+}
+
+// searchChildren is a recursive function that searches a directory tree
+// for references to dependants.
+//
+// repo is the root directory of a search. If nil is provided, the path of any
+// sub-directories that are searched is used.
+//
+// parent is the name of the current directory (just the directory name) being searched.
+func searchChildren(repo string, parent string, dependants []dependant) {
     children, err := ioutil.ReadDir(parent)
     if err != nil {
         log.Fatalf("Error reading %s: %v", parent, err)
     }
 
     if len(children) > 0 {
-        for child, i := range children {
-            searchChildren(parent, dependants)
+        for _, child := range children {
+            if strings.HasPrefix(child.Name(), ".") {
+                continue
+            }
+            newRepo := repo
+            if newRepo == "" {
+                newRepo = child.Name()
+            }
+            searchChildren(newRepo, filepath.Join(parent, child.Name()), dependants)
         }
     }
 
-    // TODO: Need to perform search here
+    bytes, err := ioutil.ReadFile(parent)
+    text := string(bytes)
+    for _, dep := range dependants {
+        if strings.Contains(text, dep.name) {
+            dep.addRepo(repo)
+        }
+    }
 }
 
 func Search(c *cli.Context) {
-    deps = c.String("deps")
-    dir = c.String("dir")
+    deps := c.String("deps")
+    dir := c.String("dir")
     if dir == "" || deps == "" {
         log.Fatal("--deps and --dir are required flags")
     }
@@ -41,5 +66,10 @@ func Search(c *cli.Context) {
         dependants = append(dependants, dependant { name: item })
     }
 
-    searchChildren(dir, &dependants)
+    searchChildren("", dir, dependants)
+    for _, dep := range dependants {
+        for _, repo := range dep.repos {
+            fmt.Printf("%s -> %s\n", repo, dep)
+        }
+    }
 }
