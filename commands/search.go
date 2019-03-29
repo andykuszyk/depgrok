f582a46baf7d9bc9bfdd609c02766496c0f2fdce
andykuszyk <andy.kuszyk@carfinance247.co.uk>
2019-03-29 14:26:06 +0000

    Refactored into smaller functions and added preliminary tests for searchChildren

--- commands/search.go
+++ commands/search.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"regexp"
 
 	"github.com/andykuszyk/depgrok/deps"
 	"github.com/urfave/cli"
@@ -22,17 +23,23 @@ func isValidParent(parent string) bool {
 	return !(strings.HasPrefix(parent, ".") || parent == "bin")
 }
 
+var secondsBetweenLoadingChars = 1.0
 func getNextLoadingCharFunc() func() string {
 	char := "|"
+	lastCharTime := time.Now()
 	return func() string {
+		if time.Now().Sub(lastCharTime).Seconds() < secondsBetweenLoadingChars {
+			return char
+		}
+		lastCharTime = time.Now()
 		switch char {
 		case "|":
 			char = "/"
 			return char
 		case "/":
-			char = "-"
+			char = "--"
 			return char
-		case "-":
+		case "--":
 			char = "\\"
 			return char
 		case "\\":
@@ -45,9 +52,87 @@ func getNextLoadingCharFunc() func() string {
 
 var getNextLoadingChar = getNextLoadingCharFunc()
 
+// Gets a FileInfo for the given path, returning it if successful, or
+// logging a fatal error if not.
+func getFileInfo(path string) os.FileInfo {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		log.Fatalf("An error occured calling os.Stat(%s): %v", path, err)
+	}
+	return fileInfo
+}
+
+// Controls whether or not searches of the file system will be parallelised.
+// By default, this is true, although it can be set to false in tests to allow
+// go routines to be debugged.
+var paralleliseSearches = true
+
+// Iterates over the children of a given parent path, calling searchChildren as a 
+// go routine on each.
+func traverseChildren(parent string, dependencies *deps.Dependencies, level int, wg *sync.WaitGroup, repo string) {
+	children, err := ioutil.ReadDir(parent)
+	if err != nil {
+		log.Fatalf("An error occured calling ioutil.ReadDir(%s): %v", parent, err)
+	}
+
+	// Traverse the children of the parent, making a recursive call to searchChildren
+	// if its a valid child.
+	for _, child := range children {
+		if !isValidParent(child.Name()) {
+			continue
+		}
+		// If this is the first traversal of the root directory, no repo name will have been
+		// provided, so extract this from the child's name.
+		newRepo := repo
+		if newRepo == "" {
+			newRepo = child.Name()
+		}
+		if paralleliseSearches {
+			go searchChildren(newRepo, filepath.Join(parent, child.Name()), dependencies, level, wg)
+		} else {
+			searchChildren(newRepo, filepath.Join(parent, child.Name()), dependencies, level, wg)
+		}
+	}
+}
+
+// Searches the file at the path parent for references to the given dependencies,
+// updating or augmenting the dependencies list as and when matches are found.
+func searchFile(parent string, repo string, dependencies *deps.Dependencies, parentInfo os.FileInfo, level int) {
+	// Interrogate the file - read its contents out as a string.
+	fmt.Fprintf(os.Stderr, "\rSearching level %d...%s", level, getNextLoadingChar())
+	bytes, err := ioutil.ReadFile(parent)
+	if err != nil {
+		log.Fatalf("Error reading file %s: %v", parent, err)
+	}
+	text := string(bytes)
+
+	// Now, iterate though each of the dependencies at the current level (in order avoid
+	// worrying about new dependencies of a higher level that have been collected on this pass)
+	// and check for a reference within the file.
+	for _, dep := range dependencies.Slice() {
+		if dep.Level != level {
+			continue
+		}
+		if stripExtension(parentInfo.Name()) != dep.Name && dep.Matches(text) {
+			dep.AddRepo(repo)
+			parentDependency := deps.Dependency{
+				Name:   stripExtension(parentInfo.Name()),
+				Parent: dep,
+				Level:  level + 1,
+			}
+			if !dependencies.Contains(parentDependency) {
+				dependencies.Add(&parentDependency)
+			}
+		}
+	}
+}
+
 // Recursively searches a file tree, amending and augmenting dependencies (at the given
 // level) as matches are discovered.
 func searchChildren(repo string, parent string, dependencies *deps.Dependencies, level int, wg *sync.WaitGroup) {
+	// Ensure that we add a counter to the waitgroup for this function call,
+	// and also wait on the "semaphore" channel to ensure too many parallel
+	// executions of this function are not taking place.
 	wg.Add(1)
 	defer wg.Done()
 	sem <- 1
@@ -56,63 +141,30 @@ func searchChildren(repo string, parent string, dependencies *deps.Dependencies,
 	}
 	defer releaseSem()
 
-	parentInfo, err := os.Stat(parent)
-	if err != nil {
-		log.Fatalf("An error occured calling os.Stat(%s): %v", parent, err)
-	}
-
 	// First, check if the parent location is a directory. If it is, traverse its children, if not
 	// interrogate its contents.
+	parentInfo := getFileInfo(parent)
 	if parentInfo.IsDir() {
-		children, err := ioutil.ReadDir(parent)
-		if err != nil {
-			log.Fatalf("An error occured calling ioutil.ReadDir(%s): %v", parent, err)
-		}
-
-		// Traverse the children of the parent, making a recursive call to searchChildren
-		// if its a valid child.
-		for _, child := range children {
-			if !isValidParent(child.Name()) {
-				continue
-			}
-			// If this is the first traversal of the root directory, no repo name will have been
-			// provided, so extract this from the child's name.
-			newRepo := repo
-			if newRepo == "" {
-				newRepo = child.Name()
-			}
-			go searchChildren(newRepo, filepath.Join(parent, child.Name()), dependencies, level, wg)
-		}
+		traverseChildren(parent, dependencies, level, wg, repo)
 	} else {
-		// Interrogate the file - read its contents out as a string.
-		fmt.Fprintf(os.Stderr, "\rSearching...%s", getNextLoadingChar())
-		bytes, err := ioutil.ReadFile(parent)
-		if err != nil {
-			log.Fatalf("Error reading file %s: %v", parent, err)
-		}
-		text := string(bytes)
-
-		// Now, iterate though each of the dependencies at the current level (in order avoid
-		// worrying about new dependencies of a higher level that have been collected on this pass)
-		// and check for a reference within the file.
-		for _, dep := range dependencies.Slice() {
-			if dep.Level != level {
-				continue
-			}
-			if dep.Matches(text) {
-				dep.AddRepo(repo)
-				parentDependency := deps.Dependency{
-					Name:   parentInfo.Name(),
-					Parent: dep,
-					Level:  level + 1,
-				}
-				if !dependencies.Contains(parentDependency) {
-					dependencies.Add(&parentDependency)
-				}
-			}
-		}
+		searchFile(parent, repo, dependencies, parentInfo, level)
 	}
 }
+
+// A closed function encapsulating the regexp to strip extensions from
+// file names.
+func stripExtensionFunc() func(string) string {
+	stripExtensionRegexp, err := regexp.Compile("(.*)\\.[a-zA-Z]+$")
+	if err != nil {
+		log.Fatalf("Error parsing strip extension regex: %v", err)
+	}
+	return func(filename string) string {
+		return stripExtensionRegexp.ReplaceAllString(filename, "$1")
+	}
+}
+
+// Strips extensions from file names.
+var stripExtension = stripExtensionFunc()
 
 func logDuration(start time.Time) {
 	fmt.Println("")
@@ -122,6 +174,9 @@ func logDuration(start time.Time) {
 
 var sem = make(chan int, runtime.NumCPU()*2)
 
+// The main function for the search command - setups up concurrency primitives and
+// initiates a search of the required depth, collecting depdencies and formating
+// them for output on the console.
 func Search(c *cli.Context) {
 	depsArg := c.String("deps")
 	dir := c.String("dir")
@@ -140,8 +195,8 @@ func Search(c *cli.Context) {
 	dependencies := deps.BuildDependencies(strings.Fields(depsArg))
 	for i := 0; i < depth; i++ {
 		searchChildren("", dir, dependencies, i, &wg)
+		wg.Wait()
 	}
-	wg.Wait()
 
 	// Print out diagrams to screen in a reasonable order.
 	for _, diagram := range dependencies.BuildDiagrams() {
--- commands/search_test.go
+++ commands/search_test.go
@@ -2,17 +2,68 @@ package commands
 
 import (
 	"testing"
+	"sync"
+	"strings"
+	"path/filepath"
+
+	"github.com/andykuszyk/depgrok/deps"
 )
 
+func TestSearchChildren_ShouldFindSimpleDependency(t *testing.T) {
+	paralleliseSearches = false
+	wg := sync.WaitGroup{}
+	dependencies := deps.BuildDependencies(strings.Fields("dependency1"))
+	searchChildren("", filepath.Join("..", "testdata"), dependencies, 0, &wg)
+	wg.Wait()
+	slice := dependencies.Slice()
+	if len(slice) != 2 {
+		t.Errorf("Expected there to be 2 dependencies, but there were %d", len(slice))
+	}
+	var dependency *deps.Dependency
+	for _, dep := range slice {
+		if dep.Name == "dependency1" {
+			dependency = dep
+			break
+		}
+	}
+	if len(dependency.Repos) != 1 {
+		t.Errorf("There should have been 1 repo for the dependency, but there were %d", len(dependency.Repos))
+	}
+	if !dependency.Repos["repo1"] {
+		var key string
+		for k, _ := range dependency.Repos {
+			key = k
+			break
+		}
+		t.Errorf("The repo should have been repo1, but it was %s", key)
+	}
+}
+
+
+func TestStripExtension_WhenTwoExtensions(t *testing.T) {
+	result := stripExtension("file.txt.txt")
+	if result != "file.txt" {
+		t.Errorf("Expected file.txt, but got %s", result)
+	}
+}
+
+func TestStripExtension_WhenOneExtension(t *testing.T) {
+	result := stripExtension("file.txt")
+	if result != "file" {
+		t.Errorf("Expected file, but got %s", result)
+	}
+}
+
 func TestGetNextLoadingChar(t *testing.T) {
+	secondsBetweenLoadingChars = 0.0
 	char := getNextLoadingChar()
 	if char != "/" {
 		t.Errorf("Expected /, but got %s", char)
 	}
 
 	char = getNextLoadingChar()
-	if char != "-" {
-		t.Errorf("Expected -, but got %s", char)
+	if char != "--" {
+		t.Errorf("Expected --, but got %s", char)
 	}
 
 	char = getNextLoadingChar()
